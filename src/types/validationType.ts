2dd0d426e5cdbb27f0dcaa888783b9840b75685b
loonlylokly <loonlylokly@gmail.com>
2024-01-16 09:01:10 +0300

    refactor: add enum valid rules

--- src/types/validationType.ts
+++ src/types/validationType.ts
@@ -1,3 +1,8 @@
 import { CombinedInput } from './inputType';
 
 export type ValidationType = Record<string, CombinedInput>;
+export enum EValidRule {
+  max = 'max',
+  min = 'min',
+  required = 'required',
+}
